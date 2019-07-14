dd53998edbce4b3d612b6d56a5bb83f0321feb33
JeramTough <1171867004@qq.com>
2019-07-15 00:54:54 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

--- src/main/java/com/jeramtough/jtcomponent/tree/processor/TreeProcessor.java
+++ src/main/java/com/jeramtough/jtcomponent/tree/processor/TreeProcessor.java
@@ -9,6 +9,10 @@ import com.jeramtough.jtcomponent.tree.structure.TreeNode;
  */
 public interface TreeProcessor {
 
+    /**
+     * @param root 是否作为根节点处理，否者的话将会向上寻找父节点，<>br</>
+     *             直到父节点为null为止才开始处理
+     */
     TreeNode processing(boolean root, TreeNodeAdapter baseTreeNodeAdapter);
 
 }
