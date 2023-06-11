df7a78b19e50d135630ba395eb560e89c6e7cc85
JeramTough <1171867004@qq.com>
2023-06-11 22:54:47 +0800

    备份代码

---
+++ src/test/java/test/util/UrlTest.java
@@ -0,0 +1,17 @@
+package test.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * <pre>
+ * Created on 2023/5/24 上午10:46
+ * by @author WeiBoWen
+ * </pre>
+ */
+public class UrlTest {
+
+    @Test
+    public void test2() {
+    }
+
+}
