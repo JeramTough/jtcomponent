268756c4aa9b16d39763dc7d606b6d5f6c399e6e
JeramTough <1171867004@qq.com>
2023-06-11 19:58:21 +0800

    更新StringUtil

--- src/main/java/com/jeramtough/jtcomponent/utils/CollectionUtil.java
+++ src/main/java/com/jeramtough/jtcomponent/utils/CollectionUtil.java
@@ -9,6 +9,7 @@ package com.jeramtough.jtcomponent.utils;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -48,10 +49,43 @@ public class CollectionUtil {
         return v != null ? v : concurrentHashMap.computeIfAbsent(key, mappingFunction);
     }
 
+
+    /**
+     * list分页
+     *
+     * @param dataList    dataList
+     * @param pageSize    pageSize
+     * @param currentPage currentPage
+     * @param <T>         T
+     * @return 分页后的list
+     */
+    public static <T> List<T> paginate(List<T> dataList, int pageSize, int currentPage) {
+        int totalElements = dataList.size();
+        int totalPages = (int) Math.ceil((double) totalElements / pageSize);
+
+        // 校正当前页码
+        if (currentPage < 1) {
+            currentPage = 1;
+        }
+        else if (currentPage > totalPages) {
+            currentPage = totalPages;
+        }
+
+        // 计算起始和结束索引
+        int startIndex = (currentPage - 1) * pageSize;
+        int endIndex = Math.min(startIndex + pageSize, totalElements);
+
+        // 截取指定范围的数据作为分页结果
+        return dataList.subList(startIndex, endIndex);
+    }
+
+    //********************
+
     private static int capacity(int expectedSize) {
         if (expectedSize < 3) {
             if (expectedSize < 0) {
-                throw new IllegalArgumentException("expectedSize cannot be negative but was: " + expectedSize);
+                throw new IllegalArgumentException(
+                        "expectedSize cannot be negative but was: " + expectedSize);
             }
             else {
                 return expectedSize + 1;
